bc4a0de2822649017b37704054de5b4ef4c49549
Ninh Ngoc Hieu <30309069+ninhngochieu@users.noreply.github.com>
2020-12-15 21:44:47 +0700

    Add new controller

---
+++ Controllers/ComboesController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using QuanLyBanHangAPI.Models;
+
+namespace QuanLyBanHangAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ComboesController : ControllerBase
+    {
+        private readonly dbContext _context;
+
+        public ComboesController(dbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Comboes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Combo>>> GetCombos([FromQuery]long? idcombo)
+        {
+            if (idcombo is null)
+            {
+                return Ok(new { status=true,data = await _context.Combos.ToListAsync()});
+            }
+            else
+            {
+                return Ok(new { status=true, data= _context.Combos.FindAsync(idcombo).Result.ListProduct});
+            }
+        }
+
+        // GET: api/Comboes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Combo>> GetCombo(long id)
+        {
+            var combo = await _context.Combos.FindAsync(id);
+
+            if (combo == null)
+            {
+                return NotFound();
+            }
+
+            return combo;
+        }
+
+        // PUT: api/Comboes/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCombo(long id, Combo combo)
+        {
+            if (id != combo.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(combo).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ComboExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Comboes
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Combo>> PostCombo([FromForm]Combo combo)
+        {
+            _context.Combos.Add(combo);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (ComboExists(combo.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Ok(new { status = true,data = CreatedAtAction("GetCombo", new { id = combo.Id }, combo) });
+        }
+
+        // DELETE: api/Comboes/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Combo>> DeleteCombo(long id)
+        {
+            var combo = await _context.Combos.FindAsync(id);
+            if (combo == null)
+            {
+                return NotFound();
+            }
+
+            _context.Combos.Remove(combo);
+            await _context.SaveChangesAsync();
+
+            return combo;
+        }
+
+        private bool ComboExists(long id)
+        {
+            return _context.Combos.Any(e => e.Id == id);
+        }
+    }
+}
